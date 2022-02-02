4ba1d87c30912244aaa74eae74807b02ac09608f
Oleegg <91334112+Oleegg@users.noreply.github.com>
2022-02-02 22:50:50 +0300

    Update index.js
    finish

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 !== 0) return false
+  const config = Object.fromEntries(bracketsConfig)
+  const steck = []
+  for (let i = 0; i < str.length; i++) {
+    for (let key in config) {
+      if (str[i] == key) {
+        if (!steck.length) {
+          steck.push(str[i])
+          // если не пустой стек
+        } else {
+          if (str[i] == config[key]) {
+            if (steck[steck.length - 1] == str[i]) {
+              steck.pop()
+            } else {
+              steck.push(str[i])
+            }
+          } else {
+            steck.push(str[i])
+          }
+        }
+        //если закрывающийся
+      } else {
+        if (str[i] == config[key] && key == steck[steck.length - 1]) {
+          steck.pop()
+        }
+      }
+    }
+  }
+  return !steck.length
 }
